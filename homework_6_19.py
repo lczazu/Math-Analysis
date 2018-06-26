8146135c27df36a85abec51a964a304404261d77
lczazu <32867127+lczazu@users.noreply.github.com>
2018-06-26 08:43:00 +0800

    Update homework_6_19.py

--- homework_6_19.py
+++ homework_6_19.py
@@ -1,11 +1,12 @@
 import numpy as np
 from math import exp
-def bina_search( fun, a, b, eps ):
+from math import floor
+
+def adjust_bina_search( fun, a, b, m ):
     """用二分法求fun函数的零根
     a，b为区间，并且fun(a) * fun(b) <= 0
-    eps为精度
+    m为精确到几位小数
     前置条件：fun在[a,b]上连续"""
-    eps = eps * 2
     
     if fun( a ) == 0:
         return a
@@ -13,7 +14,15 @@ def bina_search( fun, a, b, eps ):
         return b
     
     cou = 0
-    while b - a > eps:
+    while True:
+        ai = floor( a )
+        bi = floor( b )
+        ax = str( a ).split( '.' )
+        bx = str( b ).split( '.' )
+        
+        if( ai == bi and len( ax ) > 1 and len( bx ) > 1 and len( ax[ 1 ] ) >= m and len( bx[ 1 ] ) >= m and ax[ 1 ][ 0:m] == bx[ 1 ][ 0:m] ):
+            break
+        
         cou = cou + 1
         mid = (a + b ) / 2
         
@@ -27,24 +36,38 @@ def bina_search( fun, a, b, eps ):
             
     return ( a + b ) / 2, cou
 
-def fixed_point_ite_test( fun, x0 ):
-    """收敛性的分析"""
-    d = float( "inf" )
+
+
+def bina_search( fun, a, b, eps ):
+    """用二分法求fun函数的零根
+    a，b为区间，并且fun(a) * fun(b) <= 0
+    eps为精度
+    前置条件：fun在[a,b]上连续"""
+    eps = eps * 2
     
-    for i in range( 1000 ):
-        x1 = fun( x0 )
-        if d >= abs( x1 - x0 ):
-            d = abs( x1 - x0 )
-            x0 = x1
+    if fun( a ) == 0:
+        return a
+    if fun( b ) == 0:
+        return b
+    
+    cou = 0
+    while b - a >= eps:
+        cou = cou + 1
+        mid = (a + b ) / 2
+        
+        if fun( mid ) == 0:
+            return mid
+        
+        if fun( mid ) * fun( a ) < 0:
+            b = mid
         else:
-            return False
-    
-    return True
+            a = mid
+            
+    return ( a + b ) / 2, cou
+
+
 def fixed_point_ite( fun, x0, eps ):
     """不动点迭代法"""
-    if fixed_point_ite_test( fun, x0 ) == False:
-        return '不收敛'
-    
     x1 = fun( x0 )
     
     cou = 0
@@ -74,21 +97,19 @@ if __name__ == '__main__':
     print( '计算结果:', bina_search( fun_1, a, b, 0.05 )[ 0 ], '\n' )
     
     print( '**********\n第二题' )
-    print( '第一个公式和第三个公式的收敛性分析' )
     eps = 0.001
     x0 = 1.5
-    print( '第一个公式:', fixed_point_ite( fun_2_1, x0, eps ))
-    print( '第三个公式:',fixed_point_ite( fun_2_3, x0, eps ))
     print( '第二个公式计算的结果:')
     print( '   ', fixed_point_ite( fun_2_2, x0, eps )[ 0 ])
 
     a = 0
     b = 1
     x0 = 0
-    eps = 0.001
+    eps = 0.5 * 0.0001
+    eps = ( 1 -0.825 ) / 0.825 * eps
     print( '\n*********\n第三题' )
     print( '二分法求解' )
-    res = bina_search( fun_3_1, a, b, eps )
+    res = adjust_bina_search( fun_3_1, a, b, 3 )
     print( '近似根:', res[ 0 ], '计算次数:', res[ 1 ] )
     print( '不动点迭代' )
     res = fixed_point_ite( fun_3_2, x0, eps )
